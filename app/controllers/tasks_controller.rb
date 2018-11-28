9c3cb408929ae3a299c00e9210ec4acbb487875b
yoru <bhunjio15987@gmail.com>
2018-11-28 14:29:48 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,58 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = '正常に保存されました。'
+      redirect_to @task
+    else
+      flash.now[:danger] = '保存に失敗しました。'
+      render_to :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = '正常に保存されました。'
+      redirect_to @task
+    else
+      flash.now[:danger] = '保存に失敗しました。'
+      render_to :edit
+    end
+
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = '正常に削除されました'
+    redirect_to tasks_url
+  end
+  
+  #Strong Parameter
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
+end
